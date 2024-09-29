4c9fcd020fb39ce9fdfc393739e06bcbdbc0b256
TRASHpanda364 <dixonkimiko@gmail.com>
2024-09-29 17:14:07 -0400

    hover over img and links on img functionality

--- portfolio/src/components/Project/project.jsx
+++ portfolio/src/components/Project/project.jsx
@@ -4,14 +4,37 @@ import PropTypes from 'prop-types'
 Project.propTypes = {
     project:PropTypes.object,
     onHover:PropTypes.func,
-    target:PropTypes.string
+    target:PropTypes.string,
+    onHoverLink:PropTypes.func,
+    targetLink:PropTypes.string
 }
 
-    export default function Project({project,onHover,target}) {
+    export default function Project({project,onHover,target,onHoverLink,targetLink}) {
         return (
-            <div onMouseEnter={() => onHover(project.name)} onMouseLeave={() => onHover('')} className={target ===  project.name ? ('hover') : ('')}>
+            <div onMouseEnter={() => onHover(project.name)} onMouseLeave={() => onHover('')} className={target ===  project.name ? ('imgDiv hover') : ('imgDiv')}>
                 <img src={project.img} alt={project.name}/>
-                {target === project.name ? (<div className='project-links'><a href={project.deployed} className={target ===  project.name ? ('') : ('noHover')}>{project.name}</a><a href={project.repo} target='_blank'><ion-icon name="logo-github"></ion-icon></a></div>) : null}
+                {target === project.name ? 
+                (<div className='project-links'>
+                    <a 
+                        href={project.deployed}
+                        target='_blank' 
+                        onMouseEnter={() => onHoverLink('deployed')} 
+                        onMouseLeave={() => onHoverLink('')} 
+                        className={targetLink ===  'deployed' ? ('hoverLink') : ('noHoverLink')}
+                    >
+                        {project.name}
+                    </a>
+                    <a 
+                        href={project.repo} 
+                        target='_blank'
+                        onMouseEnter={() => onHoverLink('repo')} 
+                        onMouseLeave={() => onHoverLink('')} 
+                        className={targetLink ===  'repo' ? ('hoverLink') : ('noHoverLink')}
+                    >
+                        <ion-icon name="logo-github"></ion-icon>
+                    </a>
+                    </div>
+                ) : null}
             </div>
         )
     } 
