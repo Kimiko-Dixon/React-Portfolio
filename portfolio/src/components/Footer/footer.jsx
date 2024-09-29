5e5f86596677ad20fdc3c464589303c8fc4f6537
TRASHpanda364 <dixonkimiko@gmail.com>
2024-09-29 16:42:13 -0400

    footer hover fix

--- portfolio/src/components/Footer/footer.jsx
+++ portfolio/src/components/Footer/footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
     return (
         <div>
             <ul>
-                <li><a href="https://github.com/Kimiko-Dixon" target='_blank' onMouseEnter={() => onHover('github')} onMouseLeave={() => onHover('')} ><ion-icon  name="logo-github"></ion-icon></a></li>
+                <li><a href="https://github.com/Kimiko-Dixon" target='_blank' onMouseEnter={() => onHover('github')} onMouseLeave={() => onHover('')} className={target === 'github' ? (''): 'noHover'}><ion-icon  name="logo-github"></ion-icon></a></li>
                 <li><a href="https://www.linkedin.com/in/kimiko-a-dixon" target='_blank' onMouseEnter={() => onHover('linkedin')} onMouseLeave={() => onHover('')} className={target === 'linkedin' ? (''): 'noHover'}><ion-icon  name="logo-linkedin"></ion-icon></a></li>
             </ul>
         </div> 
