e2c39a24077bf714e212fa35552ad1fc4b9e7824
TRASHpanda364 <dixonkimiko@gmail.com>
2024-09-30 10:47:08 -0400

    header and footer styling

--- portfolio/src/pages/aboutMe.jsx
+++ portfolio/src/pages/aboutMe.jsx
@@ -1,6 +1,26 @@
+import headShot from "../img/profile-photo.jpg";
+
 export default function AboutMe() {
-    return (
-        <>
-        </>
-    )
-} 
+  return (
+    <>
+      <h2>About Me</h2>
+      <div id="head-shot">
+        <div className="content">
+          <img src={headShot} alt="Kimiko Dixon" />
+        </div>
+      </div>
+      <div id="about">
+        <div className="content">
+          <p>
+            I am a former baker who decided to take the leap into the field of
+            Technology. Although I am new to programming, I am eager to learn,
+            and with my education via the UCF Bootcamp, I assure that I will be
+            quick to pick up new information. I bring to the table the skills of
+            great communication, problem-solving, and teamwork as well as a
+            detailed oriented mindset.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
