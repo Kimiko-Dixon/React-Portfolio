aca70bdbe92fa4cf6665e318faa41abfe01e9fd8
TRASHpanda364 <dixonkimiko@gmail.com>
2024-09-30 13:44:27 -0400

    downloadable resume link

--- portfolio/src/pages/resume.jsx
+++ portfolio/src/pages/resume.jsx
@@ -1,6 +1,15 @@
 export default function Resume() {
     return (
         <>
+        <h2>Resume</h2>
+        <div>
+            <h5>Download my <a href='https://drive.google.com/uc?export=download&id=1Kzzcnlwv0wcLyEN-k2EptAJUeYcuIYsq' download>resume</a></h5>
+        </div>
+        <div id="resume">
+        <div className="content">
+          
+        </div>
+      </div>
         </>
     )
 } 
