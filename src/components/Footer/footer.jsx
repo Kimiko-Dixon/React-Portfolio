cd4491d27249f730f6382b586329c3fb3e7bd3a8
TRASHpanda364 <dixonkimiko@gmail.com>
2024-10-03 22:26:44 -0400

    bg cred

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -13,6 +13,9 @@ export default function Footer() {
                 <li><a href="https://github.com/Kimiko-Dixon" target='_blank' onMouseEnter={() => onHover('github')} onMouseLeave={() => onHover('')} className={target === 'github' ? (''): 'noHover'}><ion-icon  size="large" name="logo-github"></ion-icon></a></li>
                 <li><a href="https://www.linkedin.com/in/kimiko-a-dixon" target='_blank' onMouseEnter={() => onHover('linkedin')} onMouseLeave={() => onHover('')} className={target === 'linkedin' ? (''): 'noHover'}><ion-icon  size="large" name="logo-linkedin"></ion-icon></a></li>
             </ul>
-        </footer> 
+            <div className='background-cred'>
+                Background by <a href="https://unsplash.com/@anniespratt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Annie Spratt</a> on <a href="https://unsplash.com/photos/black-textile-in-close-up-photography-6a3nqQ1YwBw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            </div>
+        </footer>
     )
 } 
