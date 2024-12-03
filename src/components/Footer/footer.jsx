53991bcb3df721df65231745df214c558a1dfc37
Kimiko-Dixon <167892755+Kimiko-Dixon@users.noreply.github.com>
2024-12-03 09:52:32 -0500

    cleaned up files

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -11,13 +11,10 @@ export default function Footer() {
     return (
         <footer>
             <List.Root id='footer-links'>
-                <ListItem p='0'><Link href="https://github.com/Kimiko-Dixon" target='_blank' onMouseEnter={() => onHover('github')} onMouseLeave={() => onHover('')} className={target === 'github' ? (''): 'noHover'}><ion-icon  size="large" name="logo-github"></ion-icon></Link></ListItem>
-                <ListItem ><Link href="https://www.linkedin.com/in/kimiko-a-dixon" target='_blank' onMouseEnter={() => onHover('linkedin')} onMouseLeave={() => onHover('')} className={target === 'linkedin' ? (''): 'noHover'}><ion-icon  size="large" name="logo-linkedin"></ion-icon></Link></ListItem>
-                <ListItem ><Link href="mailto:dixonkimiko@gmail.com" onMouseEnter={() => onHover('email')} onMouseLeave={() => onHover('')} className={target === 'email' ? (''): 'noHover'}><ion-icon size='large' name="mail-outline"></ion-icon></Link></ListItem>
+                <ListItem p='0'><Link href="https://github.com/Kimiko-Dixon" target='_blank' onMouseEnter={() => onHover('github')} onMouseLeave={() => onHover('')} className={target === 'github' ? 'github': 'github noHover'}><ion-icon  /* size="large" */ name="logo-github"></ion-icon></Link></ListItem>
+                <ListItem ><Link href="https://www.linkedin.com/in/kimiko-a-dixon" target='_blank' onMouseEnter={() => onHover('linkedin')} onMouseLeave={() => onHover('')} className={target === 'linkedin' ? 'linkedin': 'linkedin noHover'}><ion-icon  /* size="large" */ name="logo-linkedin"></ion-icon></Link></ListItem>
+                <ListItem ><Link href="mailto:dixonkimiko@gmail.com" onMouseEnter={() => onHover('email')} onMouseLeave={() => onHover('')} className={target === 'email' ? 'email': 'email noHover'}><ion-icon /* size='large' */ name="mail-outline"></ion-icon></Link></ListItem>
             </List.Root>
-            {/* <div className='background-cred'>
-                Background by <a href="https://unsplash.com/@anniespratt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Annie Spratt</a> on <a href="https://unsplash.com/photos/black-textile-in-close-up-photography-6a3nqQ1YwBw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-            </div> */}
         </footer>
     )
 } 
--- src/components/Project/project.jsx
+++ src/components/Project/project.jsx
@@ -19,48 +19,21 @@ export default function Project({
 }) {
   return (
     <Card.Root
-      /* className="project-card" */ variant="outline"
+      variant="outline"
       shadow="-5px 10px"
       _hover={{ scale: "105%" }}
       transition="scale"
       transitionTimingFunction="ease-in-out"
       onMouseEnter={() => onHover(project.name)}
       onMouseLeave={() => onHover("")}
-      className="project-card"/* {
-        target === project.name ? "project-card hover" : "project-card"
-      } */
+      className="project-card"
     >
-      {/* <div
-        onMouseEnter={() => onHover(project.name)}
-        onMouseLeave={() => onHover("")}
-        className={target === project.name ? "hover" : null}
-      > */}
       <Image
         className="project-img"
         src={project.img}
         alt={project.name}
         p={2}
-        /* borderRadius='var(--chakra-radii-l3)' */
       />
-      {/* {target === project.name ? ( */}
-      {/* <div className="project-links">
-            <a
-              href={project.deployed}
-              target="_blank"
-              onMouseEnter={() => onHoverLink("deployed")}
-              onMouseLeave={() => onHoverLink("")}
-              className={
-                targetLink === "deployed"
-                  ? "deployed hoverLink"
-                  : "deployed noHoverLink"
-              }
-            >
-              Deployed Application 
-            </a>
-            
-          </div> */}
-      {/* ) : null} */}
-      {/* </div> */}
       <Card.Body>
         <Card.Title display='flex' alignItems='center'>
           <Link
@@ -87,47 +60,15 @@ export default function Project({
             display='flex' justifyContent='center' alignItems='center'
           >
             <ion-icon
-              /* size="large" */ className="git-logo"
+              className="git-logo"
               name="logo-github"
             ></ion-icon>
           </Link>
         </Card.Title>
-        <Card.Description>
+        <Card.Description fontSize='1.25rem'>
           {project.type}
         </Card.Description>
       </Card.Body>
     </Card.Root>
   );
-  /* return (
-           <div id='project-div'>
-                <div id='project-box'>
-                < div onMouseEnter={() => onHover(project.name)} onMouseLeave={() => onHover('')} className={target ===  project.name ? ('imgDiv hover') : ('imgDiv') }>
-                <img className= "project-img" src={project.img} alt={project.name}/>
-                {target === project.name ? 
-                (<div className='project-links'>
-                    <a 
-                        href={project.deployed}
-                        target='_blank' 
-                        onMouseEnter={() => onHoverLink('deployed')} 
-                        onMouseLeave={() => onHoverLink('')} 
-                        className={targetLink ===  'deployed' ? ('deployed hoverLink') : ('deployed noHoverLink')}
-                    >
-                        {project.name}
-                    </a>
-                    <a 
-                        href={project.repo} 
-                        target='_blank'
-                        onMouseEnter={() => onHoverLink('repo')} 
-                        onMouseLeave={() => onHoverLink('')} 
-                        className={targetLink ===  'repo' ? ('repo hoverLink') : ('repo noHoverLink')}
-                    >
-                        <ion-icon name="logo-github"></ion-icon>
-                    </a>
-                    </div>
-                ) : null}
-            </div>
-                </div>
-                
-            </div>
-        ) */
 }
